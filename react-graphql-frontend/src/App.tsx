f56ab97e65ba9d42b7b52110e070a69b9c524a2f
Filip Štiglic <filip.stiglic@aliter.com>
2019-06-10 00:00:19 +0200

    Updated react app

--- react-graphql-frontend/src/App.tsx
+++ react-graphql-frontend/src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ApolloProvider} from "react-apollo-hooks";
+import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Secured from "./components/auth/Secured";
+import PageHeaderBar from "./layout/PageHeaderBar";
+import LoginPage from "./pages/LoginPage";
+import DefaultPage from "./pages/DefaultPage";
+import SecuredPage from "./pages/secured/SecuredPage";
+
+const cache = new InMemoryCache();
+
+//Create initial state
+cache.writeData({
+    data: {
+        __typename: "data",
+        auth: {
+            __typename: "auth",
+            username: null,
+            accessToken: null,
+            refreshToken: null,
+            expirationTime: null
+        },
+        books: {}
+    }
+});
+
+const client = new ApolloClient({ cache: cache, uri: "http://localhost:8080/graphql"});
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ApolloProvider client={client}>
+          <BrowserRouter>
+              <div className="App">
+                  <PageHeaderBar/>
+                  <Switch>
+                      <Route exact={true} path="/" render={() => <DefaultPage/>}/>
+                      <Route path="/login" render={() => <LoginPage />}/>
+                      <Secured loginPath="/login">
+                          <Route path="/secured" render={() => <SecuredPage/>}/>
+                      </Secured>
+                  </Switch>
+              </div>
+          </BrowserRouter>
+      </ApolloProvider>
   );
-}
+};
 
 export default App;
